81f1ed0a514b5f5a342571667113d414699bee73
Evana Margain Puig <evanamp233@gmail.com>
2021-05-30 11:38:22 -0500

    Adds a test with screengrab

---
+++ 10-automation-tools-for-your-local-environment/projects/final/PodPlay/app/src/androidTest/java/com/raywenderlich/podplay/AutomatedUITests.kt
@@ -0,0 +1,24 @@
+package com.raywenderlich.podplay
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.raywenderlich.podplay.ui.PodcastActivity
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+
+@RunWith(AndroidJUnit4::class)
+class AutomatedUITests {
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(PodcastActivity::class.java)
+        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+    }
+
+    @Test
+    fun captureScreen() {
+        Screengrab.screenshot("podcast_activity")
+    }
+}
