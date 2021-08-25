2bb899e15fa8121f1f5543eecafcba923473fd86
Fuad Kamal <fuad@anaara.com>
2021-08-24 23:33:48 -0400

    completes encryped shared prefs

--- 06-security-best-practices/final/app/src/main/java/com/raywenderlich/android/organizedsimplenotes/MainActivityViewModel.kt
+++ 06-security-best-practices/final/app/src/main/java/com/raywenderlich/android/organizedsimplenotes/MainActivityViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
 private const val ENCRYPTED_PREFS = "ENCRYPTED_PREFS"
 private const val ENCRYPTED_PREFS_ENCRYPTION_KEY = "ENCRYPTED_PREFS_ENCRYPTION_KEY"
@@ -17,15 +19,40 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
 
   private val context by lazy { getApplication<Application>().applicationContext }
 
+  private val sharedPreferences by lazy {
+    val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    val keyEncryptionScheme = EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV
+    val valueEncryptionScheme = EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+
+    EncryptedSharedPreferences.create(
+      ENCRYPTED_PREFS,
+      masterKeyAlias,
+      context,
+      keyEncryptionScheme,
+      valueEncryptionScheme
+    )
+  }
+
   fun getEncryptionKey(): String? {
-    // TODO
-    return null
+    return sharedPreferences.getString(
+      ENCRYPTED_PREFS_ENCRYPTION_KEY,
+      null
+    )
+     null
   }
 
   fun setEncryptionKey(current: String?, new: String?) {
     if (current != getEncryptionKey()) {
       _snackbar.value = context.getString(R.string.error_current_encryption_key_incorrect)
       return
+    }
+
+    if (new.isNullOrBlank()) {
+      sharedPreferences.edit().putString(ENCRYPTED_PREFS_ENCRYPTION_KEY, null).apply()
+      _snackbar.value = context.getString(R.string.message_encryption_key_cleared)
+    } else {
+      sharedPreferences.edit().putString(ENCRYPTED_PREFS_ENCRYPTION_KEY, new).apply()
+      _snackbar.value = context.getString(R.string.message_encryption_key_set)
     }
   }
 
